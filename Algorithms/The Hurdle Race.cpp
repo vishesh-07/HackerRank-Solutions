3ea23c8c9b0af5c5c9ac61a682199bcc7773e9ac
Vishesh Suryavanshi <68236235+vishesh-07@users.noreply.github.com>
2020-11-17 14:14:44 +0530

    Create The Hurdle Race.cpp

---
+++ Algorithms/The Hurdle Race.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n, k, t, max = INT_MIN;
+
+    cin >> n >> k;
+    for(int i=0;i<n;i++) {
+        cin >> t;
+        if(t > max) {
+            max = t;
+        }
+    }
+    if(k < max) {
+        cout<<max - k;
+    }else{
+        cout<<"0";
+    }
+    return 0;
+}
