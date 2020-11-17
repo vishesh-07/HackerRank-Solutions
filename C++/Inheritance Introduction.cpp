59638d62d3763fee3c25138a7e7581ce37338e25
Vishesh Suryavanshi <68236235+vishesh-07@users.noreply.github.com>
2020-11-17 19:22:53 +0530

    Create Inheritance Introduction.cpp

---
+++ C++/Inheritance Introduction.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+class Triangle{
+    public:
+    	void triangle(){
+     		cout<<"I am a triangle\n";
+    	}
+};
+class Isosceles : public Triangle{
+    public:
+    	void isosceles(){
+    		cout<<"I am an isosceles triangle\n";
+            cout<<"In an isosceles triangle two sides are equal\n";
+    	}
+  		//Write your code here.
+};
+int main(){
+    Isosceles isc;
+    isc.isosceles();
+    isc.triangle();
+    return 0;
+}
