6b42c586b0bc0ed39c4ab9a06d7fb919d62c05b3
Vishesh Suryavanshi <68236235+vishesh-07@users.noreply.github.com>
2020-11-17 19:24:38 +0530

    Create Rectangle Area.cpp

---
+++ C++/Rectangle Area.cpp
@@ -0,0 +1,21 @@
+class Rectangle{
+  protected:
+  int width;
+  int height;
+    public:
+    void read_input()
+    {
+        cin>>width>>height;
+    }
+     void display()
+    {
+    cout<<width<<" "<<height<<"\n";
+    } 
+};
+class RectangleArea :public Rectangle{
+    public:
+      void display()
+    {
+      cout<<width * height;
+    }
+};
