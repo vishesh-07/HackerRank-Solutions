c2a528f8b630756d400dc68593bca1bf58148e8b
Vishesh Suryavanshi <68236235+vishesh-07@users.noreply.github.com>
2020-11-17 14:01:01 +0530

    Create Simple Array Sum.cpp

---
+++ Algorithms/Simple Array Sum.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n,a[1000],sum=0;
+   cin>>n;
+   for(int i=0;i<n;i++){
+       cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+     sum+=a[i];
+    }cout<<sum;
+    return 0;
+}
