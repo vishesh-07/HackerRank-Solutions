197be39fe556ee03c3ad8921f8182942bf46371b
Vishesh Suryavanshi <68236235+vishesh-07@users.noreply.github.com>
2020-11-17 14:02:15 +0530

    Create Compare the Triplets.cpp

---
+++ Algorithms/Compare the Triplets.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[5],b[5],x=0,y=0;
+    for(int i=0;i<3;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<3;i++){
+        cin>>b[i];
+    }
+    for(int i=0;i<3;i++){
+        if(a[i]>b[i]){
+            x++;
+        }else if(a[i]<b[i]){
+            y++;
+        }
+    }
+    cout<<x<<" "<<y;
+    return 0;
+}
