b9844796cac99e19cc1f0a4993868552d83e099d
Vishesh Suryavanshi <68236235+vishesh-07@users.noreply.github.com>
2020-11-18 15:04:16 +0530

    Create Extra Long Factorials.java

---
+++ Algorithms/Extra Long Factorials.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+public class Solution {
+    static void extraLongFactorials(int N) {
+            BigInteger f = new BigInteger("1");
+        for (int i = 2; i <= N; i++) 
+            f = f.multiply(BigInteger.valueOf(i)); 
+  
+        System.out.print(f);
+    }
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        extraLongFactorials(n);
+
+        scanner.close();
+    }
+}
