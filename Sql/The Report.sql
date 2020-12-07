5f8ffe793259aa44e8790253f4d999ac518e6113
Vishesh Suryavanshi <68236235+vishesh-07@users.noreply.github.com>
2020-12-07 18:05:03 +0530

    Create The Report.sql

---
+++ Sql/The Report.sql
@@ -0,0 +1,1 @@
+select if(grades.grade<8,null,students.name),grades.grade,students.marks from students join grades on students.marks between grades.min_mark and grades.max_mark order by grades.grade desc, students.name asc;
